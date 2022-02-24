36d84283ff275f8bd87ccdd5842508edc7559496
tclare <tclare@nd.edu>
2022-02-24 03:53:59 +0000

    logger: format function name spacing

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -2,17 +2,23 @@ import { formatInTimeZone } from 'date-fns-tz';
 import { getCurrentDate } from '../services/date-service';
 
 export default class Logger {
+
   getCurrentTimeFormatted(): string {
     return formatInTimeZone(getCurrentDate(), 'America/New_York', 'hh:mm:ss zz');
   }
 
+  getFunctionNameWithEllipsis(name: string) {
+    const spaces = Array(35 - name.length).join('.');
+    return `${name}${spaces}`;
+  }
+
   info(...args: any[]) {
-    console.info(`[INFO][${args[0]}][${this.getCurrentTimeFormatted()}]`, ...args.slice(1));
+    console.info(`[INFO][${this.getFunctionNameWithEllipsis(args[0])}][${this.getCurrentTimeFormatted()}]`, ...args.slice(1));
   }
   warn(...args: any[]) {
-    console.warn(`[WARN][${args[0]}][${this.getCurrentTimeFormatted()}]`, ...args.slice(1));
+    console.warn(`[WARN][${this.getFunctionNameWithEllipsis(args[0])}][${this.getCurrentTimeFormatted()}]`, ...args.slice(1));
   }
   error(...args: any[]) {
-    console.error(`[ERRO][${args[0]}][${this.getCurrentTimeFormatted()}]`, ...args.slice(1));
+    console.error(`[ERRO][${this.getFunctionNameWithEllipsis(args[0])}][${this.getCurrentTimeFormatted()}]`, ...args.slice(1));
   }
 }
